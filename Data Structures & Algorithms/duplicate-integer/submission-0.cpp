f196e5ae92b345bc58c609c79a2f506905a8d9de
Mohit Choudhary <96984129+letscodewithmohit@users.noreply.github.com>
2026-05-21 11:38:31 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+    
+    unordered_map<int,int>mp;
+
+    for(auto i : nums){
+        mp[i]++;
+    }
+
+    for(auto i : mp){
+        if(i.second >= 2){
+            return true;
+        }
+    }
+
+    return false;
+    
+    }
+};
