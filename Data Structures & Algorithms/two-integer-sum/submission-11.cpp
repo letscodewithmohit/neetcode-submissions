522032032ac446b15b2329531fcdc1050f5212d1
Mohit Choudhary <96984129+letscodewithmohit@users.noreply.github.com>
2026-06-16 15:29:16 +0530

    Add: two-integer-sum - submission-11

---
+++ Data Structures & Algorithms/two-integer-sum/submission-11.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int len = nums.size();
+        vector<int>ans;
+// 1st approch using looping 
+        // for(int i=0; i<len; i++){
+        //     for(int j = i+1; j < len; j++){
+        //         if(nums[i] + nums[j] == target){
+        //             return {i , j};
+        //         }
+        //     }
+        // }
+
+// 2nd approch using map
+
+unordered_map<int,int> mp;
+
+for(int i=0; i<len; i++){
+    int result = target - nums[i];
+
+    if(mp.find(result) != mp.end()){
+        return {mp[result],i};
+    }
+        mp[nums[i]] = i;
+    
+}
+
+}
+};
